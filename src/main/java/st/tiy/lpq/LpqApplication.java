cba3d00ee2f5b90d559848c28bd00f5f24f66893
tiyst <tiystw@gmail.com>
2023-02-22 20:49:26 +0100

    Initial commit

---
+++ src/main/java/st/tiy/lpq/LpqApplication.java
@@ -0,0 +1,13 @@
+package st.tiy.lpq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/st/tiy/lpq/LpqApplicationTests.java
@@ -0,0 +1,13 @@
+package st.tiy.lpq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
