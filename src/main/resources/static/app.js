eb99ebe083e1bf4ff0b509de0afabe6bcae1cc68
tiyst <filip@tiyst.tech>
2023-10-05 22:30:20 +0200

    MAIN-T-12 Upgraded to a new Stomp.js library

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,51 +1,110 @@
-var stompClient = null;
+//Very basic tester web app that leaves a lot to be desired
+
+let gameCode = null;
+let client;
 
 function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
+	$("#connect").prop("disabled", connected);
+	$("#disconnect").prop("disabled", !connected);
+	if (connected) {
+		$("#conversation").show();
+	} else {
+		$("#conversation").hide();
+	}
+	$("#greetings").html("");
 }
 
 function connect() {
-    var socket = new SockJS('/lpqws');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/lpq/game/1q23', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
-        });
-    });
+	if (gameCode == null || gameCode === "") {
+		return;
+	}
+
+	client = new StompJs.Client({
+		brokerURL: 'ws://localhost:8080/lpqws'
+	});
+
+	client.onConnect = () => {
+		setConnected(true);
+		let destination = "/app/lpq/" + gameCode;
+		console.log("Connected to server with game code: " + gameCode)
+		client.subscribe(destination, (message) => {
+			showGreeting(JSON.parse(message.body).content)
+		}
+		);
+
+		let connect = "/app/lpq/connect";
+		client.publish({
+			destination: connect,
+			body: JSON.stringify({
+				"gameCode": gameCode,
+				"userName": "defaultUserName"
+			})
+		});
+	}
+
+	client.onWebSocketError = (error) => {
+		console.error('Error with websocket', error);
+	};
+
+	client.activate();
 }
 
 function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
+	if (client !== null) {
+		client.deactivate();
+	}
+	setConnected(false);
+	console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/lpq/1q23", {}, $("#name").val());
-    // stompClient.send("/app/lpq", {}, JSON.stringify({'name': $("#name").val()}));
+function sendMessage() {
+	let destination = "/app/lpq/" + gameCode;
+	console.log("calling " + destination)
+	client.publish({
+		destination: destination,
+		body: JSON.stringify({
+			"playerName": "player",
+			"move": "it move it"
+		})
+	})
 }
 
 function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+	console.log("Received response: ")
+	console.log(JSON.parse(message.body))
+	$("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+async function createGame() {
+	const response = await fetch('game/create/GUESS_CHAMPION/SPLASH', {
+		method: 'POST',
+		body: JSON.stringify({"GameType": "GUESS_CHAMPION", "GuessType": "SPLASH"}),
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+	});
+	const gameData = await response.json();
+	console.log(gameData)
 }
 
 $(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+	$("form").on('submit', function (e) {
+		e.preventDefault();
+	});
+	$("#connect").click(function () {
+		connect();
+	});
+	$("#disconnect").click(function () {
+		disconnect();
+	});
+	$("#create").click(function () {
+		createGame().then();
+	})
+	$("#send").click(function () {
+		sendMessage();
+	});
+	$("#gameCode").on("change", function () {
+		gameCode = $("#gameCode").val();
+	})
 });
